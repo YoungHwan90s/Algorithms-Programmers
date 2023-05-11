af0813c55ec5c69d436752c339b1cc90401b5d86
Younghwan90s <younghwan1608@gmail.com>
2023-05-11 22:36:44 +0900

    count-p-and-y & numbers-to-array

---
+++ Level 1/count-p-and-y.js
@@ -0,0 +1,29 @@
+// function solution_first(s) {
+//   let p = 0;
+//   let y = 0;
+
+//   for (i = 0; i < s.length; i++) {
+//     if (s[i] === "p" || s[i] === "P") {
+//       p++;
+//     } else if (s[i] === "y" || s[i] === "Y") {
+//       y++;
+//     }
+//   }
+
+//   return p === y;
+// }
+
+function solution_second(s) {
+  return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
+}
+
+// function solution_third(s) {
+//   let p = s.match(/p/gi) ? s.match(/p/gi).length : 0;
+//   let y = s.match(/y/gi) ? s.match(/y/gi).length : 0;
+
+//   if (p === y) {
+//     return true;
+//   } else if (p === 0 || y === 0) {
+//     return false;
+//   } else return false;
+// }
---
+++ Level 1/numbers-to-array.js
@@ -0,0 +1,3 @@
+function solution(n) {
+    return (n+'').split('').reverse().map(num => parseInt(num))
+}
