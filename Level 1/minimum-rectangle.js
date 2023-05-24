031907a71d83a2e90e55114456def17a0158ff0d
Younghwan90s <younghwan1608@gmail.com>
2023-05-24 23:57:44 +0900

    [Revise] minimum-rectangle

--- Level 1/minimum-rectangle.js
+++ Level 1/minimum-rectangle.js
@@ -1,15 +1,19 @@
 function solution(sizes) {
-  let answer = 0;
-  let left = []
-  let right = []
-
-  for(let i = 0; i < sizes.length; i++) {
-    left.push(sizes[i][0])
-    right.push(sizes[i][1])
+    let maxWidth = 0;
+    let maxHeight = 0;
+    
+    // 가로 길이와 세로 길이 중 최대값을 구하고...
+    for (let i = 0; i < sizes.length; i++) {
+      const [width, height] = sizes[i];
+      maxWidth = Math.max(maxWidth, Math.max(width, height));
+      maxHeight = Math.max(maxHeight, Math.min(width, height));
+    }
+    
+    // 지갑의 크기 구하기
+    const walletSize = maxWidth * maxHeight;
+    
+    return walletSize;
   }
 
-return [left, right]
-  
-}
 
 console.log(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
