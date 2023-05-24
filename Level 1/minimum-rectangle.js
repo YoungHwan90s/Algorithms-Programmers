399ae177859c3f242dbd1e49e7222e7a1c496254
Younghwan90s <younghwan1608@gmail.com>
2023-05-25 00:13:34 +0900

    [Refactor] minimum-rectangle using reduce()

--- Level 1/minimum-rectangle.js
+++ Level 1/minimum-rectangle.js
@@ -1,19 +1,21 @@
-function solution(sizes) {
-    let maxWidth = 0;
-    let maxHeight = 0;
+// function solution(sizes) {
+//     let maxWidth = 0;
+//     let maxHeight = 0;
     
-    // 가로 길이와 세로 길이 중 최대값을 구하고...
-    for (let i = 0; i < sizes.length; i++) {
-      const [width, height] = sizes[i];
-      maxWidth = Math.max(maxWidth, Math.max(width, height));
-      maxHeight = Math.max(maxHeight, Math.min(width, height));
-    }
+//     // 가로 길이와 세로 길이 중 최대값을 구하고...
+//     for (let i = 0; i < sizes.length; i++) {
+//       const [width, height] = sizes[i];
+//       maxWidth = Math.max(maxWidth, Math.max(width, height));
+//       maxHeight = Math.max(maxHeight, Math.min(width, height));
+//     }
     
-    // 지갑의 크기 구하기
-    const walletSize = maxWidth * maxHeight;
-    
-    return walletSize;
-  }
+//     const walletSize = maxWidth * maxHeight;
+//     return walletSize;
+//   }
 
+function solution(sizes) {
+    const [hor, ver] = sizes.reduce(([a, b], [c, d]) => [Math.max(a, Math.max(c, d)), Math.max(b, Math.min(c, d))], [0, 0])
+    return hor * ver;
+}
 
 console.log(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
