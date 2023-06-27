849d507bf48cd618f23794a568ef9f7d5c615a9d
Younghwan90s <younghwan1608@gmail.com>
2023-06-27 13:43:36 +0900

    [Algorithm] get-day from a & b

--- Level 1/get-day.js
+++ Level 1/get-day.js
@@ -1,13 +1,6 @@
 // 2016년 1월 1일 금요일
 // a, b를 입력 받아 2016년 a월 b일이 무슨 요일인지 return
 
-// function solution(a, b) {
-//     const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-//     const date = new Date(`2016-${a}-${b}`);
-//     const dayOfWeek = date.getDay();
-//     return days[dayOfWeek];
-//   }
-
 function solution(a, b) {
   const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
   const date = new Date(`2016=${a}-${b}`);
