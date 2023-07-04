cbca98402c0114f49b58d3e8deb8b0d82478808d
Younghwan90s <younghwan1608@gmail.com>
2023-07-04 23:45:56 +0900

    [C#] minus-two-numbers

---
+++ C#-Prac/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution
+{
+    public static int solution(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+
+    public static void Main()
+    {
+        int num1 = 10;
+        int num2 = 3;
+        int result = solution(num1, num2);
+        Console.WriteLine(result);
+    }
+}
